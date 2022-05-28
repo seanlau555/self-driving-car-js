bb45ff6d6aa46c91d8fd5119b756783a0dc8128c
Sean Lau <lthon0225@gmail.com>
2022-05-28 14:12:41 +0800

    part 1 for the js, car and road setup

---
+++ controls.js
@@ -0,0 +1,47 @@
+class Controls {
+	constructor() {
+		this.forward = false;
+		this.left = false;
+		this.right = false;
+		this.reverse = false;
+
+		this.#addKeyboardListeners() // # means private func
+	}
+
+	#addKeyboardListeners() {
+		document.onkeydown = (evt) => {
+			switch(evt.key) {
+				case "ArrowLeft":
+					this.left = true;  // => func refer this. to the constructor one
+					break;
+				case "ArrowRight":
+					this.right = true;
+					break;
+				case "ArrowUp":
+					this.forward = true;
+					break;
+				case "ArrowDown":
+					this.reverse = true;
+					break;
+			}
+			console.table(this)
+		}
+		document.onkeyup = (evt) => {
+			switch(evt.key) {
+				case "ArrowLeft":
+					this.left = false;
+					break;
+				case "ArrowRight":
+					this.right = false;
+					break;
+				case "ArrowUp":
+					this.forward = false;
+					break;
+				case "ArrowDown":
+					this.reverse = false;
+					break;
+			}
+		}
+	}
+}
+
